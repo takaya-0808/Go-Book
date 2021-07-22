bcd2c9d2980961e741591eb7ed6202dd06461fb2
takaya <takaya1912@gmail.com>
2021-07-22 23:50:04 +0900

    add GET & POST

---
+++ app/service/book.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"Go-Book/app/model"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type BookService struct {
+	SqlHandler
+}
+
+func (bs *BookService) SetBook(book *model.Book) (id int, err error) {
+
+	result, err := bs.Conn.Exec(
+		"Insert into books (title, content) values (?, ?)", book.Title, book.Content,
+	)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ID64, err := result.LastInsertId()
+	if err != nil {
+		return
+	}
+	id = int(ID64)
+	return
+}
+
+func (bs *BookService) GetBookList() []model.Book {
+
+	rows, err := bs.Conn.Query("select * from books")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var books []model.Book
+	for rows.Next() {
+		book := model.Book{}
+		if err := rows.Scan(&book.Id, &book.Title, &book.Content); err != nil {
+			panic(err)
+		}
+		books = append(books, book)
+	}
+	return books
+}
+
+func (bs *BookService) UpdateBook(book *model.Book) error {
+
+	bookUpdate, err := bs.Conn.Prepare("update books set title=? and context=? where id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer bookUpdate.Close()
+
+	result, err := bookUpdate.Exec(&book.Title, &book.Content, &book.Id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rowsAffect, err := result.RowsAffected()
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Panicln(rowsAffect)
+	return err
+}
+
+func (bs *BookService) DeleteBook(id int) error {
+	bookDelete, err := bs.Conn.Prepare("delete from books where id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer bookDelete.Close()
+
+	result, err := bookDelete.Exec(id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rowsAffect, err := result.RowsAffected()
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Println(rowsAffect)
+	return err
+}
